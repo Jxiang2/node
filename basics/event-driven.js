9b3753646ad75d77811dcd9f2c3060073f482118
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-02 17:13:19 -0700

    node intro

---
+++ basics/event-driven.js
@@ -0,0 +1,35 @@
+const EventEmitter = require('events');
+const celebrity = new EventEmitter();
+
+// subscribe1 to celebrity for observer1
+celebrity.on('race', (result)=>{
+    if (result === 'win')
+        console.log('Congrats!')
+});
+
+// subscribe2 to celebrity for observer1
+celebrity.on('race', (result)=>{
+    if (result === 'loss')
+        console.log('Oh sh*t!')
+});
+
+// subscribe to celebrity for observer2
+celebrity.on('race', (result)=>{
+    if (result === 'win')
+        console.log('I could have win!')
+});
+
+// subscribe2 to celebrity for observer2
+celebrity.on('race', (result)=>{
+    if (result === 'loss')
+        console.log('Nice!')
+});
+
+// emit events
+celebrity.emit('race', 'win');
+celebrity.emit('race', 'loss');
+
+// trigger when no more code to excute
+process.on('exit', (code)=>{
+    console.log('Process exit with code: ', code);
+});
---
+++ basics/hello.js
@@ -0,0 +1,8 @@
+const mission = process.argv[2]
+
+if (mission === 'learn') {
+    console.log('Time to write node code');
+} else {
+    console.log(`Is ${mission} readlly more fun?`);
+}
+
