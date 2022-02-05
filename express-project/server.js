71985ce8e6b4b3ff293702d3863b955fd23e679e
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-05 11:39:42 -0700

    understood express

---
+++ express-project/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+const friends = [
+	{ id: 0, name: "Newton" },
+	{ id: 1, name: "Einstein" },
+];
+
+app.get("/", (req, res) => {
+	res.json({ message: "Welcome!!!" });
+});
+
+app.get("/friends", (req, res) => {
+	res.status(200).json(friends);
+});
+
+app.get("/friends/:id", (req, res) => {
+	const friendID = Number(req.params.id);
+	const friend = friends[friendID];
+	if (friend) {
+		res.json(friend);
+	} else {
+		res.status(404).json({
+			error: "Friend does not exist",
+		});
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}...`);
+});
