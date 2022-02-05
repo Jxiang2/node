fff86d7aa487e022495e2e43b57d7a0a4b6bccad
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-05 13:35:52 -0700

    organize with router

--- express-project/server.js
+++ express-project/server.js
@@ -8,13 +8,13 @@ const app = express();
 const PORT = 3000;
 
 // common middlewares
-app.use(express.json());
 app.use((req, res, next) => {
 	const start = Date.now();
 	next();
 	const delta = Date.now() - start;
 	console.log(`${req.method} ${req.baseUrl}${req.url}... finished in ${delta}ms`);
 });
+app.use(express.json());
 
 // routers
 app.use("/friends", friendsRouter);
