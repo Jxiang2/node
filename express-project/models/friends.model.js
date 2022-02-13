fe9539e7009bf09f565c85164b50a3cbe9ac27aa
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-13 13:55:39 -0700

    done

--- express-project/models/friends.model.js
+++ express-project/models/friends.model.js
@@ -1,6 +1,7 @@
 const friends = [
 	{ id: 0, name: "Newton" },
 	{ id: 1, name: "Einstein" },
+	{ id: 2, name: "Mask" },
 ];
 
 module.exports = friends;
--- express-project/server.js
+++ express-project/server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 	const start = Date.now();
 	next();
 	const delta = Date.now() - start;
-	console.log(`${req.method} ${req.baseUrl}${req.url}... finished in ${delta}ms`);
+	console.log(`${req.method} ${req.baseUrl}${req.url} ... finished in ${delta}ms`);
 });
 app.use(express.json()); // convert every json/application request body to JS object
 
