378e1751bc145f854a28728456c5f26d7121aa64
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-15 22:21:34 -0700

    ts node project initializer

--- ts-express/src/app.ts
+++ ts-express/src/app.ts
@@ -1,6 +1,6 @@
 import express, {Application, Request, Response, NextFunction} from "express";
 
-const app: express.Application = express()
+const app: Application = express()
 
 const add = (a: number, b: number): number => {
     return a+b
