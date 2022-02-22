dde2964c3a51fefbde7c6ebae1afac5454471092
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-21 21:52:56 -0700

    setup dist

--- ts-nasa-project/server/src/server.ts
+++ ts-nasa-project/server/src/server.ts
@@ -4,6 +4,10 @@ import app, { express } from "./expressApp";
 const PORT = process.env.PORT || 8000;
 const server: http.Server  = http.createServer(app);
 
+app.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.send('Hello')
+})
+
 server.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}...`);
 });
