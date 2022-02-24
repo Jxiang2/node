ea9ddc849e453dd69a8fd38b8e86e4cbee0f1f14
Jingyu Xiang <jxiang2@ualberta.ca>
2022-02-23 18:10:58 -0700

    integrated planet data

--- ts-nasa-project/server/src/models/planets.model.ts
+++ ts-nasa-project/server/src/models/planets.model.ts
@@ -1,3 +1,40 @@
-const planets: object = [];
+// read csv
+// readable stream -> pipe -> writable stream
 
-export { planets }
+import { parse } from "csv-parse";
+import fs from "fs";
+
+interface Planet {
+    koi_disposition: string;
+    koi_insol: number;
+    koi_prad: number;
+}
+
+const planets: object[] = [];
+const isHabitablePlanet = <T extends Planet>(planet: T) => {
+	return (
+		planet["koi_disposition"] === "CONFIRMED" &&
+		planet["koi_insol"] > 0.36 &&
+		planet["koi_insol"] < 1.11 &&
+		planet["koi_prad"] < 1.6
+	);
+};
+
+fs.createReadStream("./data/kepler_data.csv")
+	.pipe(
+		parse({
+			comment: "#",
+			columns: true,
+		})
+	)
+	.on("data", (data) => {
+		isHabitablePlanet(data) && planets.push(data);
+	})
+	.on("error", (err) => {
+		console.log(err.message);
+	})
+	.on("end", () => {
+		console.log(`${planets.length} habitable planets found!`);
+	});
+
+export { planets };
