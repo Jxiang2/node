ef6d3b85889569fa6bfc8198add9c47d96fa6ad1
Jingyu Xiang <jxiang2@ualberta.ca>
2022-03-07 00:40:31 -0700

    add morgan

--- ts-nasa-project/server/src/app.ts
+++ ts-nasa-project/server/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import cors from "cors";
 import { planetRouter } from "./routes/planets/planets.routers";
 import path from "path";
@@ -6,13 +7,14 @@ import path from "path";
 const app: express.Application = express();
 
 // cors options
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:3000'];
 const options: cors.CorsOptions = {
   origin: allowedOrigins
 };
 
 // settings
 app.use(cors(options));
+app.use(morgan("combined"));
 app.use(express.json());
 
 // server client
@@ -22,7 +24,11 @@ app.use(express.static(path.join(__dirname, "..", "dist", "client")));
 app.use(planetRouter);
 
 // set home page to react's home page
+// when running dev mode, epxect a frontend page with no data
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist", "client", "index.html"));
+});
+app.get('/launch', (req, res) => {
   res.sendFile(path.join(__dirname, "..", "dist", "client", "index.html"));
 });
 
