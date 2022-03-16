e84a6052e2ab62c47047a9a56e63e1180d5f8d3f
Jingyu Xiang <jxiang2@ualberta.ca>
2022-03-15 23:58:39 -0600

    remove ts-nasa-project

--- ts-nasa-project/server/src/routes/planets/planets.router.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from "express";
-import { httpgetAllPlanets } from "./planets.controller";
-
-const planetRouter: Router = express.Router();
-
-planetRouter.get('/', httpgetAllPlanets);
-
-export { planetRouter };
