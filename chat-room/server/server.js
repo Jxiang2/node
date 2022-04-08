bd25f390c826716b25d451d0e31368cf1e301575
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-07 21:30:20 -0600

    debug

--- chat-room/server/server.js
+++ chat-room/server/server.js
@@ -2,14 +2,13 @@ const http = require("http");
 const socketio = require("socket.io");
 
 const sockets = require("./sockets");
-const app = require("./app");
+const app = require("./expreeApp");
 
 const PORT = 3000 || process.env.PORT;
 
 // create a server & add socket on it
 const server = http.createServer(app);
 const io = socketio(server, { cors: { origin: "*" } });
-
 
 // run server
 server.listen(PORT, () => console.log("server running ..."));
