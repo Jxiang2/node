a701ae65eb31e0857723b04c3df6d24565480ccb
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-09 21:26:15 -0600

    done

--- nodeHttpServer.js
+++ nodeHttpServer.js
@@ -18,19 +18,23 @@ const server = http.createServer((req, res) => {
 			friends.push(JSON.parse(readable));
 		});
 		req.pipe(res); //response will end afeter request is end
-	} else if (req.method === "GET" && items[1] === "friends") {
+	}
+	else if (req.method === "GET" && items[1] === "friends") {
 		res.setHeader("Content-Type", "application/json");
 		if (items.length === 3) {
 			if (Number(items[2]) >= friends.length) {
 				res.statusCode = 404;
 				res.end("no such friend");
-			} else {
+			}
+			else {
 				res.end(JSON.stringify(friends[Number(items[2])]));
 			}
-		} else {
+		}
+		else {
 			res.end(JSON.stringify(friends));
 		}
-	} else {
+	}
+	else {
 		res.statusCode = 404;
 		res.end("invalid url");
 	}
