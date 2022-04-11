784b07dbb290e34e1097c1783107dfdc9ef27aca
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-11 11:41:15 -0600

    customise buffer split method

--- nodeBufferSplit.js
+++ nodeBufferSplit.js
@@ -1,3 +1,24 @@
 /**
  * Buffer split method implementation
  */
+
+Buffer.prototype.split = function (sep) {
+  let sepLen = Buffer.from(sep).length;
+  let result = [];
+  let start = 0;
+  let offset = 0;
+
+  while (this.indexOf(sep, start) !== -1) {
+    offset = this.indexOf(sep, start);
+    result.push(this.slice(start, offset));
+
+    // skip the seperator
+    start = offset + sepLen;
+  }
+  return result;
+};
+
+// test
+let buf = Buffer.from("hello ! world ! there");
+let bufArr = buf.split(" ! ");
+bufArr.map((e) => console.log(e.toString()));
