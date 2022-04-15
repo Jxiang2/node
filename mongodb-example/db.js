b82df1159b6c71cd88139415b2e9ae1ffe1f522a
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-15 01:34:23 -0600

    mongodb done

---
+++ mongodb-example/db.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+
+let dbConnection;
+
+module.exports = {
+  connectToDB: (cb) => {
+    MongoClient.connect("mongodb+srv://rootUser:123@cluster0.9os83.mongodb.net/bookstore?retryWrites=true&w=majority")
+      .then(client => {
+        dbConnection = client.db();
+        return cb();
+      }).catch(err => {
+        console.log(err);
+        return cb(err);
+      });
+  },
+  getDB: () => dbConnection
+};
