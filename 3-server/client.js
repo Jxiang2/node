e17eb0fef03623471f9685751ed16646d49a00af
Jingyu Xiang <jxiang2@ualberta.ca>
2022-04-16 11:39:42 -0600

    server example done

---
+++ 3-server/client.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+// http.get({
+//   host: 'localhost',
+//   port: 1234, // port point of resource
+//   path: '/?a=1',
+// }, (res) => {
+
+// });
+
+const options = {
+  host: 'localhost',
+  port: 1234, // port point of resource
+  path: '/?a=1',
+  method: "POST",
+  headers: {
+    "Content-type": "application/json"
+  }
+};
+
+let req = http.request(options, (res) => {
+  let arr = [];
+  res.on("data", (data) => {
+    arr.push(data);
+  });
+
+  res.on("end", () => {
+    console.log(Buffer.concat(arr).toLocaleString());
+  });
+});
+req.end('{"name": "lg"}');
