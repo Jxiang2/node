334f510fe5d6ed8feb641a40115a69ed72119fb4
Jxiang2 <xjyhenry@gmail.com>
2022-06-15 10:29:32 -0400

    review ts

--- gql-course/frontend/src/index.js
+++ gql-course/frontend/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "http://localhost:4000/graphql",
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
