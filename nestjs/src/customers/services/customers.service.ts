2996618149fd96e6de7539c1b7ebd4401e6974e4
Jxiang2 <xjyhenry@gmail.com>
2023-06-12 14:18:14 -0400

    add

---
+++ nestjs/src/customers/services/customers.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { CreateCustomerDto } from "../dtos/CreateCustomer.dto";
+import { Customer } from "../types/Customer";
+
+@Injectable()
+export class CustomersService {
+  private readonly customers: Customer[] = [
+    { id: 1, email: "www.abc.com", createdAt: new Date(), name: "John Doe" },
+    { id: 2, email: "www.def.com", createdAt: new Date(), name: "Jack Man" },
+    { id: 3, email: "www.ghi.com", createdAt: new Date(), name: "Peter Pan" },
+    { id: 4, email: "www.jkl.com", createdAt: new Date(), name: "Mary Jane" },
+  ];
+
+  public findCustomer(id: number) {
+    return this.customers.find((customer) => customer.id === id);
+  }
+
+  public getCustomers() {
+    return this.customers;
+  }
+
+  public createCustomer(payload: CreateCustomerDto) {
+    this.customers.push({
+      ...payload,
+      createdAt: new Date(),
+    });
+  }
+}
