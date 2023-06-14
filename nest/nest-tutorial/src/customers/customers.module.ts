589f6ebb7eb857f1e2c5b5940cd2d43d59c1b85c
Jxiang2 <xjyhenry@gmail.com>
2023-06-13 21:17:48 -0400

    add

--- nest/nest-tutorial/src/customers/customers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { CustomersController } from "customers/controllers/customers.controller";
-import { CustomersService } from "./services/customers.service";
-
-@Module({
-  controllers: [CustomersController],
-  providers: [CustomersService],
-})
-export class CustomersModule {}
--- nest/nest-tutorial/src/customers/dtos/CreateCustomer.dto.ts
+++
@@ -1,7 +0,0 @@
-import { Customer } from "../types/Customer";
-
-export class CreateCustomerDto implements Omit<Customer, "createdAt"> {
-  id: number;
-  name: string;
-  email: string;
-}
